af3d6f42da601c786197e44f3405193333a89d74
Jonelle Adona <jonelle.adona@knights.ucf.edu>
2021-05-23 19:18:58 -0700

    add php

---
+++ contact.php
@@ -0,0 +1,15 @@
+<?php
+
+if (isset($_POST['submit'])) {
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $message = $_POST['message'];
+  // $submit = $_POST['submit'];
+
+  $mailTo = "jonelle.adona@knights.ucf.edu";
+  $headers = "From: ".$email;
+  $txt = "You have recieved an email from ".$name.".\n\n".$message;
+
+  mail($mailTo, $text, $headers );
+  header("Location: index.php?mailsend")
+}
